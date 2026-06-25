38da31f70d53bfe10992c352569990cb0fbc1fef
Hemen Solomon <fiyorixoxo@gmail.com>
2026-06-25 11:56:34 +0300

    update shapes

--- shapes/shape.py
+++ shapes/shape.py
@@ -1,24 +1,22 @@
-from transform.transform import Transform
+class Tool:
+    LINE     = "LINE"
+    POLYLINE = "POLYLINE"
+    POLYGON  = "POLYGON"
+    SELECT   = "SELECT"
+
 
 class Shape:
-    def __init__(self, color=(1, 0, 0)):
-        self.vertices = []
-        self.color = color
-        self.transform = Transform()  # Stores tx, ty, rotation, sx, sy
-        self.selected = False
+    def __init__(self, stype: str, pts: list, color: tuple,
+                 lw: float, filled: bool):
+        self.type   = stype
+        self.pts    = pts        
+        self.color  = color
+        self.lw     = lw
+        self.filled = filled
 
-    def draw(self):
-        raise NotImplementedError("Subclasses must implement draw()")
-
-    def apply_opengl_transforms(self):
-        
-        from OpenGL.GL import glTranslatef, glRotatef, glScalef
-        
-        # 1. Move the shape's coordinate space
-        glTranslatef(self.transform.tx, self.transform.ty, 0.0)
-        
-        # 2. Rotate around the Z-axis (2D rotation)
-        glRotatef(self.transform.rotation, 0.0, 0.0, 1.0)
-        
-        # 3. Scale the coordinate space
-        glScalef(self.transform.sx, self.transform.sy, 1.0)
+        # Affine transform state 
+        self.tx:    float = 0.0
+        self.ty:    float = 0.0
+        self.angle: float = 0.0   
+        self.sx:    float = 1.0
+        self.sy:    float = 1.0
